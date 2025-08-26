b88cd10586bcb56a4df35543902854388892ef2a
Akshay singh <singhakshaychauhan015@gmail.com>
2025-08-26 23:40:36 +0530

    Fix import case sensitivity for asyncHandler in user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,4 @@
-import asyncHandler from '../utils/asyncHandler.js';
-
-
-
+import asyncHandler from '../utils/asynchandler.js';
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({ 
         message: 'User registered successfully' 
